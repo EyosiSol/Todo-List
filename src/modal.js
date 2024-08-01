9a47ef0cf84507ec6c853cf9931a92650c596a09
EyosiSol <eyosila123@gmail.com>
2024-08-01 23:14:36 +0300

    working on the modal

--- src/modal.js
+++ src/modal.js
@@ -1,19 +1,53 @@
-const openModal = document.getElementById('addtask');
+const openModal = document.getElementById("addtask");
 const modal = document.getElementById("modal");
-const closeModal = document.getElementById('cancel');
-const AddTask = document.getElementById('Add');
+const closeModal = document.getElementById("cancel");
+const AddTask = document.getElementById("Add");
 
-const taskbar = document.querySelector(".taskbar");
+export default function mymodal() {
+  openModal.addEventListener("click", () => {
+    modal.showModal();
+  });
 
-export default function mymodal(){
-    openModal.addEventListener('click', () => {
-        modal.showModal();
-    });
+  closeModal.addEventListener("click", () => {
+    modal.close();
+  });
 
-    closeModal.addEventListener('click', () => {
-        modal.close();
-    });
+  AddTask.addEventListener("click", () => {
 
-};
+    const taskform = document.getElementById("taskform");
+    
+    const taskbar = document.querySelector(".taskbar");
+    const Title = document.getElementById("title");
+    const Description = document.getElementById("Desc");
+    const DueDate = document.getElementById("date");
 
+    taskbar.innerHTML = `
+        <div class="task">
+                <div class="taskleft">
+                    <div class="taskTitle">
+                        <input type="checkbox">${Title.value}:
+                    </div>
+                    <div class="taskDesc">
+                        ${Description.value}
+                    </div>
+                </div>
+                <div class="taskRight">
+                    <div class="taskDate">${DueDate.value}</div>
+                    <div class="buttons">
+                        <button id="favbtn"><img src="../src/assets/star_filled.svg" alt=""></button>
+                        <button id="editbtn"><img src="../src/assets/edit.svg" alt=""></button>
+                        <button id="Delbtn"><img src="../src/assets/delete.svg" alt=""></button>
+                    </div>
+                </div>
+            </div>
+        `;
+    // mainbar = document.querySelector('.mainbar');
 
+    // mainbar.appendChild(taskbar);
+    
+    taskform.reset();
+
+    modal.close();
+    
+  });
+}
