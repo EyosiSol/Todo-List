b1320f40aab99ef38602ab03cc558a59788031c3
Sis Mac <eyosila123@gmail.com>
2024-08-08 20:09:05 +0300

    reworking on the modal logic

--- src/modal.js
+++ src/modal.js
@@ -1,53 +1,31 @@
-const openModal = document.getElementById("addtask");
-const modal = document.getElementById("modal");
-const closeModal = document.getElementById("cancel");
-const AddTask = document.getElementById("Add");
+export default function modal(){
 
-export default function mymodal() {
-  openModal.addEventListener("click", () => {
-    modal.showModal();
-  });
+const openModal = document.querySelector('#addtask');
+const closeModal = document.querySelector('#cancel');
+const modal = document.querySelector('#modal');
+const submit = document.querySelector('#Add');
 
-  closeModal.addEventListener("click", () => {
-    modal.close();
-  });
+openModal.addEventListener('click',()=>{
+  modal.showModal();
+})
+closeModal.addEventListener('click',()=>{
+  modal.close();
+})
+submit.addEventListener('click',()=>{
+  const myForm = document.querySelector('#taskform');
 
-  AddTask.addEventListener("click", () => {
+  const myTitle = document.querySelector('#title');
+  const myDesc = document.querySelector('#Desc');
+  const myDate = document.querySelector('#date');
 
-    const taskform = document.getElementById("taskform");
-    
-    const taskbar = document.querySelector(".taskbar");
-    const Title = document.getElementById("title");
-    const Description = document.getElementById("Desc");
-    const DueDate = document.getElementById("date");
 
-    taskbar.innerHTML = `
-        <div class="task">
-                <div class="taskleft">
-                    <div class="taskTitle">
-                        <input type="checkbox">${Title.value}:
-                    </div>
-                    <div class="taskDesc">
-                        ${Description.value}
-                    </div>
-                </div>
-                <div class="taskRight">
-                    <div class="taskDate">${DueDate.value}</div>
-                    <div class="buttons">
-                        <button id="favbtn"><img src="../src/assets/star_filled.svg" alt=""></button>
-                        <button id="editbtn"><img src="../src/assets/edit.svg" alt=""></button>
-                        <button id="Delbtn"><img src="../src/assets/delete.svg" alt=""></button>
-                    </div>
-                </div>
-            </div>
-        `;
-    // mainbar = document.querySelector('.mainbar');
+  console.log(`${myTitle.value} and ${myDesc.value} and ${myDate.value}`);
+  
 
-    // mainbar.appendChild(taskbar);
-    
-    taskform.reset();
 
-    modal.close();
-    
-  });
+  myForm.reset();
+
+  modal.close();
+  
+})
 }
